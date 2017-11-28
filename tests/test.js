4cf1f946ea2c7b4cdb8e0b60d3cdd3c94f37217f
Elias Hantula <ehantula@indiana.edu>
2017-11-28 17:08:58 -0500

    first

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,33 @@
 // Your code here ...
 
-module.exports = {};
+module.exports = {
+  'Home test': browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#pageIndex', 1000)
+      .assert.containsText('h1', 'New User')
+      .end();
+  },
+
+
+ 'User creation': browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#pageIndex', 1000)
+      .saveScreenshot('screenshots/1.jpg')
+      .setValue('input[name="fname"]', '')
+      .setValue('input[name="lname"]', '')
+      .setValue('input[name="email"]', '')
+      .saveScreenshot('screenshots/2.jpg')
+      .submitForm('#userForm')
+      .waitForElementVisible('#pageShow', 3000)
+      .assert.containsText('#firstName', '')
+      .assert.containsText('#lastName', '')
+      .assert.containsText('#email', '')
+      .saveScreenshot('screenshots/3.jpg')
+      .end();
+  }
+
+
+
+};
